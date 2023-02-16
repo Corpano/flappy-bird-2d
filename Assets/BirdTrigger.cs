28d9988818d0ffb73bafd84d8abbb17fcf661430
ZacharyH777 <93843297+ZacharyH777@users.noreply.github.com>
2023-02-15 18:16:03 -0800

    Final Draft
    Flappy bird

--- Assets/BirdTrigger.cs
+++ Assets/BirdTrigger.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class BirdTrigger : MonoBehaviour
 {
+
+    public Animator anim; //Animator for the GameOver sign
+
+    public GameObject restartTrigger; //Restarts the game
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         // If we lost, don't do nothing
@@ -18,10 +23,15 @@ public class BirdTrigger : MonoBehaviour
             transform.eulerAngles = Vector3.forward * -50f;
             // Lose
             FindObjectOfType<GameManager>().Lose();
+            // Game Over Animation
+            anim.SetBool("isDead",true);
+
+            restartTrigger.SetActive(true);
         }
         // Bird should get a point
         else if (other.CompareTag("Success"))
         {
+            Destroy(other.gameObject);
             FindObjectOfType<GameManager>().OnScore();
         }
     }
--- Assets/Scripts/BirdScripts/Scripts/StartTest.cs
+++ Assets/Scripts/BirdScripts/Scripts/StartTest.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//temp class to start game
 public class StartTest : MonoBehaviour
 {
     public GameObject RedBird;
+    public GameObject Menu;
+    public GameObject pipeSpawner;
+    public GameObject scoreControl;
+    
+    public bool OnStart = false;
     // Start is called before the first frame update
+
     void Start()
     {
-        RedBird.SetActive(true);
+        OnStart = true;
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && OnStart == true)
+        {
+            RedBird.SetActive(true);
+            Menu.SetActive(false);
+            pipeSpawner.SetActive(true);
+            scoreControl.SetActive(true);
+            OnStart = false;
+        }
     }
 }
