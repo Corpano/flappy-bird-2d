e2886bdeea7734167edff334bce235421bc8e4b7
Anto Pinjatic <34194750+Corpano@users.noreply.github.com>
2023-02-15 17:38:24 -0800

    Score Text, Game Manager Changes

--- Assets/BirdTrigger.cs
+++ Assets/BirdTrigger.cs
@@ -5,11 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class BirdTrigger : MonoBehaviour
 {
-    // Sound for bird when it gets a point
-    public AudioSource scoreSoundEffect;
-    // Sound for when the bird hits pipe/ground
-    public AudioSource hitSoundEffect;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
         // If we lost, don't do nothing
@@ -21,16 +16,13 @@ public class BirdTrigger : MonoBehaviour
         {
             // Rotate it downwards
             transform.eulerAngles = Vector3.forward * -50f;
-            // Hit sound effect
-            hitSoundEffect.Play();
             // Lose
             FindObjectOfType<GameManager>().Lose();
         }
         // Bird should get a point
         else if (other.CompareTag("Success"))
         {
-            // Score sound effect
-            scoreSoundEffect.Play();
+            FindObjectOfType<GameManager>().OnScore();
         }
     }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     // Did we lose?
     public static bool didLose = false;
+
+    // Sound for bird when it gets a point
+    public AudioSource scoreSoundEffect;
+    // Sound for when the bird hits pipe/ground
+    public AudioSource hitSoundEffect;
+
+    public TextMeshProUGUI pointsTxt;
+
+    public int PointsCount = 0;
 
     private void Start()
     {
@@ -23,9 +33,21 @@ public class GameManager : MonoBehaviour
     {
         didLose = true;
 
+        // Hit sound effect
+        hitSoundEffect.Play();
+
         // Stop moving environment
         PipeMover.isMoving = false;
         GroundMover.isMoving = false;
         EnvironmentMover.isMoving = false;
+    }
+
+    public void OnScore()
+    {
+        // Score sound effect
+        scoreSoundEffect.Play();
+        
+        PointsCount++;
+        pointsTxt.text = PointsCount.ToString("00");
     }
 }
